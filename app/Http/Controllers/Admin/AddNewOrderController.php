d62d7219360b7867c8658f546b7a8edc0c73e54e
sayyadkhan5774900 <59308228+sayyadkhan5774900@users.noreply.github.com>
2021-05-12 13:40:01 +0500

    first

--- app/Http/Controllers/Admin/AddNewOrderController.php
+++ app/Http/Controllers/Admin/AddNewOrderController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Requests\StoreOrderRequest;
+use App\Models\Order;
+use App\Models\User;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class AddNewOrderController extends Controller
 {
@@ -14,5 +18,23 @@ class AddNewOrderController extends Controller
         abort_if(Gate::denies('add_new_order_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return view('admin.addNewOrders.index');
+    }
+
+    public function store(StoreOrderRequest $request)
+    {
+        $order = Order::create($request->all());
+
+        $order->client_id = auth()->user()->id;
+        $order->save();
+
+        if ($request->input('file', false)) {
+            $order->addMedia(storage_path('tmp/uploads/' . basename($request->input('file'))))->toMediaCollection('file');
+        }
+
+        if ($media = $request->input('ck-media', false)) {
+            Media::whereIn('id', $media)->update(['model_id' => $order->id]);
+        }
+
+        return redirect('/admin');
     }
 }
---
+++ resources/views/admin/clientActiveOrders/show.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.show') }} {{ trans('cruds.order.title') }}
+    </div>
+
+    <div class="card-body">
+        <div class="form-group">
+            <div class="form-group">
+                <a class="btn btn-default" href="{{ route('admin.client-active-orders.index') }}">
+                    {{ trans('global.back_to_list') }}
+                </a>
+            </div>
+            <table class="table table-bordered table-striped">
+                <tbody>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.id') }}
+                        </th>
+                        <td>
+                            {{ $order->id }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.service') }}
+                        </th>
+                        <td>
+                            {{ App\Models\Order::SERVICE_SELECT[$order->service] ?? '' }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.description') }}
+                        </th>
+                        <td>
+                            {!! $order->description !!}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.address') }}
+                        </th>
+                        <td>
+                            {{ $order->address }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.city') }}
+                        </th>
+                        <td>
+                            {{ $order->city }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.postcode') }}
+                        </th>
+                        <td>
+                            {{ $order->postcode }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.contact') }}
+                        </th>
+                        <td>
+                            {{ $order->contact }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.file') }}
+                        </th>
+                        <td>
+                            @if($order->file)
+                                <a href="{{ $order->file->getUrl() }}" target="_blank">
+                                    {{ trans('global.view_file') }}
+                                </a>
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.meeting') }}
+                        </th>
+                        <td>
+                            <input type="checkbox" disabled="disabled" {{ $order->meeting ? 'checked' : '' }}>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.client') }}
+                        </th>
+                        <td>
+                            {{ $order->client->name ?? '' }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.service_provider') }}
+                        </th>
+                        <td>
+                            {{ $order->service_provider->name ?? '' }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.order.fields.status') }}
+                        </th>
+                        <td>
+                            {{ App\Models\Order::STATUS_SELECT[$order->status] ?? '' }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="form-group">
+                <a class="btn btn-default" href="{{ route('admin.client-active-orders.index') }}">
+                    {{ trans('global.back_to_list') }}
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
