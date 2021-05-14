e9a0a3311101273842a5b624f1ba78e4ff1e81a7
sayyadkhan5774900 <59308228+sayyadkhan5774900@users.noreply.github.com>
2021-05-14 17:35:41 +0500

    chanfing my services cruds

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -19,14 +19,14 @@ class PermissionRoleTableSeeder extends Seeder
         Role::findOrFail(2)->permissions()->sync($admin_permissions);
 
         $client_permissions = $supper_admin_permissions->filter(function ($permission) {
-            $client_permissions_array = ['order_create', 'order_edit', 'order_delete', 'order_show', 'services_dropdown_access','add_new_order_access','client_active_order_access','profile_password_edit'];
+            $client_permissions_array = ['order_create', 'order_edit' , 'order_update', 'order_delete', 'order_show', 'services_dropdown_access','add_new_order_access','client_active_order_access','profile_password_edit'];
             return in_array($permission->title, $client_permissions_array) && substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
         });
         Role::findOrFail(3)->permissions()->sync($client_permissions);
 
 
         $provider_permissions = $supper_admin_permissions->filter(function ($permission) {
-            $provider_permissions_array = ['add_new_service_access','my_service_access','profile_password_edit','service_create','service_edit','service_delete', 'service_show'];
+            $provider_permissions_array = ['add_new_service_access','my_service_access','profile_password_edit','service_create','service_edit' ,'service_update','service_delete', 'service_show'];
             return in_array($permission->title, $provider_permissions_array) && substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
         });
         Role::findOrFail(4)->permissions()->sync($provider_permissions);
